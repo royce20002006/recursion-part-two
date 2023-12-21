401454d71dace5caeb97d9d971405d1612131605
Adam <adamhall327@gmail.com>
2023-12-20 21:48:30 -0700

    prob 5 complete

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -13,10 +13,35 @@ reverse(""); // ""
 ***********************************************************************/
 
 
-function reverse(str) {
-  // Your code here 
+// function reverse(str, n = str.length - 1, reversed = '') {
+
+//   if (str.length === 0) return reversed;
+
+//   if (n === 0) {
+//     return reversed + str[n];
+//   }
+//   reversed += str[n];
+//   return reverse(str, n - 1, reversed);
+// };
+
+function reverse(str, i = str.length - 1, res = '') {
+
+  if (i < 0) {
+    return res
+  }
+  res += str.slice(i,i + 1)
+ return reverse(str, i - 1, res)
+
 }
 
+
+
+reverse("house"); // "esuoh"
+reverse("dog"); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
